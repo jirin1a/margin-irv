0d27c9372a5855cdae92b1761d2a43b7b2213982
Jiri Navratil <jiri@us.ibm.com>
2022-05-09 10:31:40 -0600

    wip

--- run_utah.sh
+++ run_utah.sh
@@ -1,2 +1,4 @@
 #for i in data/profile_format/*txt; do echo "$i"; ./analyzeirv -task 0 -ballots $i -logfile logs/task0/log.$(echo $i|sed "s/.*\///g").txt -debug; done
-for i in data/profile_format/*txt; do echo "$i"; ./analyzeirv -task 1 -ballots $i -logfile logs/task1/log.$(echo $i|sed "s/.*\///g").txt -debug; done
+#for i in data/profile_format/*txt; do echo "$i"; ./analyzeirv -task 1 -ballots $i -logfile logs/task1/log.$(echo $i|sed "s/.*\///g").txt -debug; done
+#for i in data/profile_format/*txt; do echo "$i"; ./analyzeirv -task 2 -ballots $i -logfile logs/task2/log.$(echo $i|sed "s/.*\///g").txt -debug; done
+for i in data/profile_format/*txt; do echo "$i"; ./analyzeirv -task 3 -ballots $i -logfile logs/task3/log.$(echo $i|sed "s/.*\///g").txt -debug; done
